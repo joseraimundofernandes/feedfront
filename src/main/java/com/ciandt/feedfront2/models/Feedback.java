25da5aeeb374a74185c187b3896fc3e60dc8bc08
José Raimundo <joseff@ciandt.com>
2022-08-17 15:23:39 -0300

    adicionado get e set para oqueMelhora para corrigir serializacao

--- src/main/java/com/ciandt/feedfront2/models/Feedback.java
+++ src/main/java/com/ciandt/feedfront2/models/Feedback.java
@@ -1,6 +1,5 @@
 package com.ciandt.feedfront2.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
@@ -26,7 +25,7 @@ public class Feedback {
     @Length(min = 3, message = "A descrição deve ter mais de 2 caracteres")
     private String descricao;
     @Column
-    private String oQueMelhora;
+    private String oqueMelhora;
     @Column
     private String comoMelhora;
     @Column(nullable = false)
@@ -47,13 +46,21 @@ public class Feedback {
         setDescricao(descricao);
     }
 
-    public Feedback(LocalDate data, Employee autor, Employee proprietario, String descricao, String oQueMelhora, String comoMelhora) {
+    public Feedback(LocalDate data, Employee autor, Employee proprietario, String descricao, String oqueMelhora, String comoMelhora) {
         setData(data);
         setAutor(autor);
         setProprietario(proprietario);
         setDescricao(descricao);
-        setOQueMelhora(oQueMelhora);
+        setoqueMelhora(oqueMelhora);
         setComoMelhora(comoMelhora);
+    }
+
+    private void setoqueMelhora(String oqueMelhora) {
+        this.oqueMelhora = oqueMelhora;
+    }
+
+    private String getoqueMelhora(String oqueMelhora) {
+        return this.oqueMelhora;
     }
 
 }
